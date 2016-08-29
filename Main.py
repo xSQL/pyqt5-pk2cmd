f7ad01fcfa44990141d662a2357218bded09e1be
Sergey Logoshko <logoshko.s@gmail.com>
2016-08-29 21:22:32 +0000

    pep8

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 __author__ = 'mediator'
 
 from PyQt5.QtWidgets import QApplication, qApp, QFileDialog, QGraphicsScene
@@ -9,8 +8,10 @@ from PyQt5.QtGui import QIcon
 import sys, menu, area
 
 
-class Window():
+class Window(object):
+    """..."""
     def __init__(self):
+        """..."""
         self.app = QApplication(sys.argv)
         self.ui = loadUi('main.ui')
         self.ui.setWindowTitle('PicKit2 simple programmer')
@@ -19,6 +20,7 @@ class Window():
         self.app.setWindowIcon(QIcon("icon/icon.png"))
 
     def openHex(self):
+        """..."""
         file = QFileDialog.getOpenFileName()
         with open(file[0], 'r') as f:
             content = f.readlines()
@@ -28,11 +30,14 @@ class Window():
             data = line[8:-1]
             list = [data[i:i+2] for i in range(0, len(data), 2)]
             list = list + ['FF' for i in range(0, 17-len(list))]
-            chars = '</span><span style="width: 32px; margin-left: 10px;">'.join(list)
-            s = '<b style="color: red">%s:</b> <span style="margin-left: 10px; width: 32px">%s</span>'%(line[2:6], chars)
+            s_tags= '</span><span style="width: 32px; margin-left: 10px;">'
+            chars = s_tags.join(list)
+            s = '<b style="color: red">%s:</b>'\
+            '<span style="margin-left: 10px; width: 32px">%s</span>'%(
+                line[2:6],
+                chars
+            )
             self.ui.textBrowser.append(s)
-
-
 
 
 w = Window()
